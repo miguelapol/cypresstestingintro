9415f86b921e89e61b8156f51d565c4d5c9650ef
miguelapol <apolonio1995@gmail.com>
2022-06-27 13:37:27 -0500

    haciendo buenas practicas

--- cypress/e2e/primer_test.spec.cy.js
+++ cypress/e2e/primer_test.spec.cy.js
@@ -4,6 +4,6 @@ describe('Carga la pagina principal', () => {
   it('carga la página principal', () => {
     cy.visit('http://127.0.0.1:5501/Curso%20JS%20Moderno/52-Testing-Cypress/index.html');
     cy.contains('h1','Administrador de Pacientes de Veterinaria');
-    cy.get('h1').should('exist');
+    cy.get('[data-cy="titulo-proyecto"]').should('exist');
   });
 });
