cd91e6816383ca85bc42a342f29b968a9c0dde7d
miguelapol <apolonio1995@gmail.com>
2022-06-27 13:39:20 -0500

    verificando el elemento y su texto

--- cypress/e2e/primer_test.spec.cy.js
+++ cypress/e2e/primer_test.spec.cy.js
@@ -3,7 +3,8 @@
 describe('Carga la pagina principal', () => {
   it('carga la página principal', () => {
     cy.visit('http://127.0.0.1:5501/Curso%20JS%20Moderno/52-Testing-Cypress/index.html');
-    cy.contains('h1','Administrador de Pacientes de Veterinaria');
+    
+    cy.contains('[data-cy="titulo-proyecto"]','Administrador de Pacientes de Veterinaria');
     cy.get('[data-cy="titulo-proyecto"]').should('exist');
   });
 });
