f6c679b6cfda8c9931582b53288482ab009dc06a
miguelapol <apolonio1995@gmail.com>
2022-06-28 12:54:40 -0500

    las respectivas pruebas al crear paciente

---
+++ cypress/e2e/crear_cita.spec.cy.js
@@ -0,0 +1,26 @@
+///<reference types="cypress"/>
+
+describe('Carga la pagina principal test de creacion', () => {
+    it('carga la página principal', () => {
+    cy.visit('/index.html');
+    cy.get('[data-cy="mascota-input"]')
+     .type('Luna');
+     cy.get('[data-cy="propietario-input"]')
+     .type('Miguel');
+     cy.get('[data-cy="telefono-input"]')
+     .type('55986628');
+     cy.get('[data-cy="fecha-input"]')
+     .type('2002-11-25')
+     cy.get('[data-cy="hora-input"]')
+     .type('10:30')
+     cy.get('[data-cy="sintomas-input"]')
+     .type('EL perrito se siente bastante bien')
+
+     cy.get('[data-cy="submit-cita"]')
+     .click();
+     cy.get('[data-cy="citas-heading"]')
+     .invoke('text')
+     .should('equal','Administra tus Citas')
+    
+    });
+  });
