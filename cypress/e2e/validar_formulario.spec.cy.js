8c9a354d34c015ad99a19687981bd95e680028aa
miguelapol <apolonio1995@gmail.com>
2022-06-28 11:50:23 -0500

    creando validacion de formulario

---
+++ cypress/e2e/validar_formulario.spec.cy.js
@@ -0,0 +1,9 @@
+///<reference types="cypress"/>
+
+describe('Carga la pagina principal', () => {
+    it('carga la página principal', () => {
+      cy.visit('http://127.0.0.1:5501/Curso%20JS%20Moderno/52-Testing-Cypress/index.html');
+     
+  
+    });
+  });
