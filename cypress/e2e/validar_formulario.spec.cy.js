8a8b2a985491199bf08f234a4509ddd94791ad71
miguelapol <apolonio1995@gmail.com>
2022-06-28 11:56:10 -0500

    haciendo pruebas del submit

--- cypress/e2e/validar_formulario.spec.cy.js
+++ cypress/e2e/validar_formulario.spec.cy.js
@@ -3,7 +3,8 @@
 describe('Carga la pagina principal', () => {
     it('carga la página principal', () => {
       cy.visit('http://127.0.0.1:5501/Curso%20JS%20Moderno/52-Testing-Cypress/index.html');
-     
-  
+    
+      cy.get('[data-cy="formulario"]')
+        .submit();
     });
   });
