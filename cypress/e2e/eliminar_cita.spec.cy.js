af8595af28c31a367a933e562c6568ea2213c1c8
miguelapol <apolonio1995@gmail.com>
2022-06-30 12:57:53 -0500

    Pruebas para eliminar

---
+++ cypress/e2e/eliminar_cita.spec.cy.js
@@ -0,0 +1,37 @@
+///<reference types="cypress"/>
+
+describe('Llena los campos para una nueva cita y la elimina', () => {
+    it('campos nueva cita', () => {
+    cy.visit('/index.html');
+    cy.get('[data-cy="mascota-input"]')
+     .type('Luna');
+     cy.get('[data-cy="propietario-input"]')
+     .type('Miguel');
+     cy.get('[data-cy="telefono-input"]')
+     .type('55986628');
+     cy.get('[data-cy="fecha-input"]')
+     .type('2002-11-25')
+     cy.get('[data-cy="hora-input"]')
+     .type('10:30')
+     cy.get('[data-cy="sintomas-input"]')
+     .type('EL perrito se siente bastante bien')
+
+     cy.get('[data-cy="submit-cita"]')
+     .click();
+     cy.get('[data-cy="citas-heading"]')
+     .invoke('text')
+     .should('equal','Administra tus Citas')
+     cy.get('[data-cy="alerta"]')
+     .invoke('text')
+     .should('equal','Se agregó correctamente')
+
+     cy.get('[data-cy="alerta"]')
+     .should('have.class','alert-success') 
+    });
+    it('Eliminar una cita',()=>{
+        cy.get('[data-cy="btn-eliminar"]')
+        .click();
+    })
+
+
+  });
