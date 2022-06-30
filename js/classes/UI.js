b68e47a92bba566c00637cb165a3f7e7b81bb214
miguelapol <apolonio1995@gmail.com>
2022-06-30 12:18:26 -0500

    modificaciones en lÃa alerta

--- js/classes/UI.js
+++ js/classes/UI.js
@@ -8,6 +8,10 @@ class UI {
     }
 
     imprimirAlerta(mensaje, tipo) {
+        const alertaPrevia=document.querySelector('.alert');
+        if(alertaPrevia){
+            alertaPrevia.remove();
+        }
         // Crea el div
         const divMensaje = document.createElement('div');
         divMensaje.classList.add('text-center', 'alert', 'd-block', 'col-12');
@@ -80,8 +84,8 @@ class UI {
             // Añade un botón de editar...
             const btnEditar = document.createElement('button');
             btnEditar.onclick = () => cargarEdicion(cita);
-
-
+            //Dataset de cu´res
+            btnEditar.dataset.cy='btn-editar';
             btnEditar.classList.add('btn', 'btn-info');
             btnEditar.innerHTML = 'Editar <svg fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"></path></svg>'
 
