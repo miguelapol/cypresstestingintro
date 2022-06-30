f6ad536aeff456672280aacba117efc75c77b6ee
miguelapol <apolonio1995@gmail.com>
2022-06-30 12:57:31 -0500

    añadiendo date cy

--- js/classes/UI.js
+++ js/classes/UI.js
@@ -77,7 +77,7 @@ class UI {
             // Agregar un botón de eliminar...
             const btnEliminar = document.createElement('button');
             btnEliminar.onclick = () => eliminarCita(id); // añade la opción de eliminar
-
+            btnEliminar.dataset.cy='btn-eliminar';
             btnEliminar.classList.add('btn', 'btn-danger', 'mr-2');
             btnEliminar.innerHTML = 'Eliminar <svg fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>'
 
